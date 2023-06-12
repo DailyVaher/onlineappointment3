f30697d6d359f3cd916fe60506a685c68cfde4f4
DailyVaher <daily.vaher@vikk.ee>
2023-06-12 12:50:39 +0300

    Integration test updated [Delivers #185349569]

--- cypress/e2e/integrationTest.cy.js
+++ cypress/e2e/integrationTest.cy.js
@@ -1,16 +1,17 @@
 describe('Adding appointments as existing user', () => {
     it('should log in a user and create a new appointment successfully', () => {
-        cy.visit('http://localhost:4000');
-        cy.get('button').contains('Sign In').click();
-        cy.get('#signInEmail').type('admin');
-        cy.get('#signInPassword').type('admin123');
-        cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-secondary').click();
+        cy.visit('http://localhost:4000/');
+        cy.get('button').contains('Sign In').click({force: true});
         cy.wait(2000);
-        cy.get('button').contains('Add Appointment').click();
-        cy.get('#addAppointmentTitle').type('Title');
-        cy.get('#addAppointmentContent').type('Content');
-        cy.get('#addAppointmentModal > .modal-dialog > .modal-content > .modal-footer > .btn-success').click({ force: true });
+        cy.get('#signInEmail').type('admin')
+        cy.get('#signInPassword').type('KollneKollne')
+        cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-secondary').click({force: true});
         cy.wait(2000);
-        cy.get('.appointment-card').should('contain', 'Title');
+        cy.get('button').contains('Add Appointment').click({force: true});
+        cy.wait(2000);
+        cy.get('#addAppointmentTitle').type('Appointment 1', {force: true})
+        cy.get('#addAppointmentContent').type('This is an example of an appointment', {force: true})
+        cy.get('#addAppointmentModal > .modal-dialog > .modal-content > .modal-footer > .btn-secondary').click({force: true});
+        cy.wait(2000);
     });
 });
